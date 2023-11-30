c90271c711fd58a3fac6e00420e605e416481178
ShariarSheikh <shariar.dev@gmail.com>
2023-11-30 11:14:46 +0600

    update tsconfig

--- app/core/ErrorHandler.ts
+++ app/core/ErrorHandler.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
-import ApiResponse from '../core/ApiResponse'
+import ApiResponse from './ApiResponse'
 
 //------------------------------------------------
 
